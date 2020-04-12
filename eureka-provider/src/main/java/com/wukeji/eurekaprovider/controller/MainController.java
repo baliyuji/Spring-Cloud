17fd9db00d5b8d617ddca2dffb6f538f8d5f0f67
wukeji <985943616@qq.com>
2020-04-12 20:36:39 +0800

    添加服务提供者

---
+++ eureka-provider/src/main/java/com/wukeji/eurekaprovider/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.wukeji.eurekaprovider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/getHi")
+    public String getHi(){
+        return "hi";
+    }
+
+}
